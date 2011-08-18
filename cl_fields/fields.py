c869c3b600d1bc6c7270257d5e06e7b87bc4da0f
Gert Van Gool <gertvangool@gmail.com>
2011-08-18 14:29:54 +0200

    PEP8

--- cl_fields/fields.py
+++ cl_fields/fields.py
@@ -2,11 +2,13 @@ from django.db import models
 from django import forms
 from django.utils import simplejson as json
 
+
 class JSONWidget(forms.Textarea):
     def render(self, name, value, attrs=None):
         if not isinstance(value, basestring):
             value = json.dumps(value, indent=2)
         return super(JSONWidget, self).render(name, value, attrs)
+
 
 class JSONFormField(forms.CharField):
     def __init__(self, *args, **kwargs):
@@ -19,13 +21,17 @@ class JSONFormField(forms.CharField):
         try:
             return json.loads(value)
         except Exception, exc:
-            raise forms.ValidationError(u'JSON decode error: %s' % (unicode(exc),))
+            raise forms.ValidationError(
+                u'JSON decode error: %s' % (unicode(exc),)
+            )
+
 
 class JSONField(models.TextField):
     __metaclass__ = models.SubfieldBase
 
     def formfield(self, **kwargs):
-        return super(JSONField, self).formfield(form_class=JSONFormField, **kwargs)
+        return super(JSONField, self).formfield(form_class=JSONFormField,
+                                                **kwargs)
 
     def to_python(self, value):
         if isinstance(value, basestring):
