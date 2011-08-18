9942fd3a2cb767a105aa694f2c440d3ffeeb8c79
Gert Van Gool <gertvangool@gmail.com>
2011-08-18 15:11:34 +0200

    Forgotten import

--- cl_fields/fields.py
+++ cl_fields/fields.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.utils import simplejson as json
+from django.utils.translation import ugettext_lazy as _
 
 
 class JSONWidget(forms.Textarea):
